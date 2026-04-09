08b3f6b8bf6a374ed21aa54ea1e7ee51e42d58ef
Sunil kumar <sunilkumar3020@gmail.com>
2026-04-09 10:07:16 +0530

    Login controller created

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -7,14 +7,14 @@ export const register = async (req, res) => {
     const { name, email, phone, password } = req.body;
     try {
         if (!name || !email || !phone || !password) {
-            res.status(400).json({ message: "Please fill required fields." })
+            return res.status(400).json({ message: "Please fill required fields." })
         }
 
         // check user exist or not
 
         const userExist = await User.findOne({ email })
         if (userExist) {
-            res.status(400).json({ message: "User already exists" })
+            return res.status(400).json({ message: "User already exists" })
         }
 
         // hashed password
@@ -40,3 +40,34 @@ export const register = async (req, res) => {
 
 
 //login user controller
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // find user
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.send(400).json({ message: "User not found" })
+        }
+
+        // check password
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+
+        // token generate
+
+        const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: "1d" })
+
+        res.json({ token })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Server error" })
+    }
+}
